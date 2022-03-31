d57e60d0e8fe5df4a1c640beaee8182993558338
fengzhengzhan <1343831868@qq.com>
2022-03-31 08:17:31 +0800

    build.sh

--- Programs/demo/build.sh
+++ Programs/demo/build.sh
@@ -5,25 +5,8 @@ then
 	PROGRAMNAME=$2
 	PROGRAMNAME_TRACE="${PROGRAMNAME}_trace"
 	PROGRAMNAME_PASS="${PROGRAMNAME}_pass"
-	echo "ProgramName: <${PROGRAMNAME}>"
 	
-	# Using clang sanitizer to hook functions.
-	cd ../..  # Return to the root path.
-
-	cd llvm_mode/ClangSanitizer
-	make
-	cd ../..
-
-	# Building the LLVM Pass project.
-	cd llvm_mode/Build
-	cmake ../Transforms
-	make
-	cd ../..
-
-
-	# Compile the program
 	PROGRAMS="Programs"
-
 	SOURCES="code_sources"
 	IR="code_IR"
 	BIN="code_Bin"
@@ -32,28 +15,14 @@ then
 	LINE_SAVE="data_graph/binaryline.info"
 	SANPATH="llvm_mode/ClangSanitizer"
 	SUFFIX="bc"
-
-
-	cd ${PROGRAMS}/${PROGRAMNAME}
-	clang++ -g -emit-llvm -c ${SOURCES}/${PROGRAMNAME}.cc -o ${IR}/${PROGRAMNAME}.${SUFFIX} # IR
-	clang++ -fsanitize-recover=address -fsanitize-coverage=trace-pc-guard,trace-cmp -emit-llvm -c ${IR}/${PROGRAMNAME}.${SUFFIX} -o ${IR}/${PROGRAMNAME_TRACE}.${SUFFIX} 
-	rm -f ${LINE_SAVE}
-	echo "{" >> ${LINE_SAVE}
-	opt -load ../../${LLVMPASSPATH} -line -S ${IR}/${PROGRAMNAME_TRACE}.${SUFFIX} -o ${IR}/${PROGRAMNAME_PASS}.${SUFFIX} >> ${LINE_SAVE}
-	echo "}" >> ${LINE_SAVE}
-	llc -filetype=obj ${IR}/${PROGRAMNAME_PASS}.${SUFFIX} -o ${IR}/${PROGRAMNAME}.o  # Object file
-	clang++ -fsanitize=address -Wl,--whole-archive -L../../${SANPATH} -lcmpcov -Wl,--no-whole-archive ${IR}/${PROGRAMNAME}.o -o ${BIN}/${PROGRAMNAME}  # Link
-	cd ../..
-
-
-	echo -e "\n------- ${PROGRAMNAME} -------"
-	# Run
-	./${PROGRAMS}/${PROGRAMNAME}/${BIN}/${PROGRAMNAME} -f "${PROGRAMS}/${PROGRAMNAME}/seeds_init/init.seed"
-	# ./${PROGRAMS}/${PROGRAMNAME}/${BIN}/${PROGRAMNAME} -f "${PROGRAMS}/${PROGRAMNAME}/seeds_crash/final.seed"
-
-
+	
+	echo "ProgramName: <${PROGRAMNAME}>"
+	
+	# Using clang sanitizer to hook functions.
+	cd ../..  # Return to the root path.
+	
 	# Clear files.
-	if [ $3 == "-rm" ]
+	if [ $3 ] && [ $3 == "-rm" ]
 	then
 		echo "-rm"
 		echo "rm -f ./${PROGRAMS}/${PROGRAMNAME}/${IR}/${PROGRAMNAME_TRACE}.${SUFFIX}"
@@ -65,7 +34,7 @@ then
 		rm -f ./${PROGRAMS}/${PROGRAMNAME}/${IR}/${PROGRAMNAME}.o
 		rm -f ./${PROGRAMS}/${PROGRAMNAME}/${BIN}/${PROGRAMNAME}
 
-	elif [ $3 == "-rma" ]
+	elif [ $3 ] && [ $3 == "-rma" ]
 	then
 		echo "-rma"
 		echo "${SANPATH}  make clean"
@@ -82,9 +51,50 @@ then
 		rm -f ./${PROGRAMS}/${PROGRAMNAME}/${IR}/${PROGRAMNAME_PASS}.${SUFFIX}
 		rm -f ./${PROGRAMS}/${PROGRAMNAME}/${IR}/${PROGRAMNAME}.o
 		rm -f ./${PROGRAMS}/${PROGRAMNAME}/${BIN}/${PROGRAMNAME}
+	
+	else
+		cd llvm_mode/ClangSanitizer
+		make
+		cd ../..
+
+		# Building the LLVM Pass project.
+		cd llvm_mode/Build
+		cmake ../Transforms
+		make
+		cd ../..
+
+
+		# Compile the program
+		cd ${PROGRAMS}/${PROGRAMNAME}
+		clang++ -g -emit-llvm -c ${SOURCES}/${PROGRAMNAME}.cc -o ${IR}/${PROGRAMNAME}.${SUFFIX} # IR
+		clang++ -fsanitize-recover=address -fsanitize-coverage=trace-pc-guard,trace-cmp -emit-llvm -c ${IR}/${PROGRAMNAME}.${SUFFIX} -o ${IR}/${PROGRAMNAME_TRACE}.${SUFFIX} 
+		rm -f ${LINE_SAVE}
+		echo "{" >> ${LINE_SAVE}
+		opt -load ../../${LLVMPASSPATH} -line -S ${IR}/${PROGRAMNAME_TRACE}.${SUFFIX} -o ${IR}/${PROGRAMNAME_PASS}.${SUFFIX} >> ${LINE_SAVE}
+		echo "}" >> ${LINE_SAVE}
+		llc -filetype=obj ${IR}/${PROGRAMNAME_PASS}.${SUFFIX} -o ${IR}/${PROGRAMNAME}.o  # Object file
+		clang++ -fsanitize=address -Wl,--whole-archive -L../../${SANPATH} -lcmpcov -Wl,--no-whole-archive ${IR}/${PROGRAMNAME}.o -o ${BIN}/${PROGRAMNAME}  # Link
+		cd ../..
+		
+		# Run
+		if [ $3 ]
+		then
+			echo -e "\n------- ${PROGRAMNAME} -------"
+			if [ $3 == "seedinit" ]
+			then
+				./${PROGRAMS}/${PROGRAMNAME}/${BIN}/${PROGRAMNAME} -f "${PROGRAMS}/${PROGRAMNAME}/seeds_init/init.seed"
+			elif [ $3 == "seedfinal" ]
+			then
+				./${PROGRAMS}/${PROGRAMNAME}/${BIN}/${PROGRAMNAME} -f "${PROGRAMS}/${PROGRAMNAME}/seeds_crash/final.seed"
+			else
+				echo -e "Error Parameters."
+			fi
+		fi
+		
 	fi
+
 else
-	echo "Usage: ./build.sh [-n <program_name>] [-rm] [-rma]"
+	echo "Usage: ./build.sh [-n <program_name> <runseeds>] [-rm] [-rma]"
 fi
 
 
