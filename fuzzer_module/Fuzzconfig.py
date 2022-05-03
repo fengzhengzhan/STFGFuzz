d554e0c942ffa5643507c4b56a6d9ef72aeb12fd
fengzhengzhan <1343831868@qq.com>
2022-05-03 10:11:14 +0800

    Mutator_bytes

--- fuzzer_module/Fuzzconfig.py
+++ fuzzer_module/Fuzzconfig.py
@@ -102,21 +102,26 @@ FLAG_DICT = {
 # Hex for visible characters
 # bytes.fromhex(HEX_ASCII[n])
 # bytes.fromhex("".join(HEX_ASCII[n1:n2])
-HEX_ASCII = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '0a', '0b', '0c', '0d', '0e', '0f', '10', '11',
-             '12', '13', '14', '15', '16', '17', '18', '19', '1a', '1b', '1c', '1d', '1e', '1f', '20', '21', '22', '23',
-             '24', '25', '26', '27', '28', '29', '2a', '2b', '2c', '2d', '2e', '2f', '30', '31', '32', '33', '34', '35',
-             '36', '37', '38', '39', '3a', '3b', '3c', '3d', '3e', '3f', '40', '41', '42', '43', '44', '45', '46', '47',
-             '48', '49', '4a', '4b', '4c', '4d', '4e', '4f', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
-             '5a', '5b', '5c', '5d', '5e', '5f', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '6a', '6b',
-             '6c', '6d', '6e', '6f', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '7a', '7b', '7c', '7d',
-             '7e', '7f', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '8a', '8b', '8c', '8d', '8e', '8f',
-             '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '9a', '9b', '9c', '9d', '9e', '9f', 'a0', 'a1',
-             'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'aa', 'ab', 'ac', 'ad', 'ae', 'af', 'b0', 'b1', 'b2', 'b3',
-             'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'ba', 'bb', 'bc', 'bd', 'be', 'bf', 'c0', 'c1', 'c2', 'c3', 'c4', 'c5',
-             'c6', 'c7', 'c8', 'c9', 'ca', 'cb', 'cc', 'cd', 'ce', 'cf', 'd0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7',
-             'd8', 'd9', 'da', 'db', 'dc', 'dd', 'de', 'df', 'e0', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9',
-             'ea', 'eb', 'ec', 'ed', 'ee', 'ef', 'f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'fa', 'fb',
-             'fc', 'fd', 'fe', 'ff']
+BYTES_ASCII = [b'\x00', b'\x01', b'\x02', b'\x03', b'\x04', b'\x05', b'\x06', b'\x07', b'\x08', b'\t', b'\n', b'\x0b',
+               b'\x0c', b'\r', b'\x0e', b'\x0f', b'\x10', b'\x11', b'\x12', b'\x13', b'\x14', b'\x15', b'\x16', b'\x17',
+               b'\x18', b'\x19', b'\x1a', b'\x1b', b'\x1c', b'\x1d', b'\x1e', b'\x1f', b' ', b'!', b'"', b'#', b'$',
+               b'%', b'&', b"'", b'(', b')', b'*', b'+', b',', b'-', b'.', b'/', b'0', b'1', b'2', b'3', b'4', b'5',
+               b'6', b'7', b'8', b'9', b':', b';', b'<', b'=', b'>', b'?', b'@', b'A', b'B', b'C', b'D', b'E', b'F',
+               b'G', b'H', b'I', b'J', b'K', b'L', b'M', b'N', b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W',
+               b'X', b'Y', b'Z', b'[', b'\\', b']', b'^', b'_', b'`', b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h',
+               b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y',
+               b'z', b'{', b'|', b'}', b'~', b'\x7f', b'\x80', b'\x81', b'\x82', b'\x83', b'\x84', b'\x85', b'\x86',
+               b'\x87', b'\x88', b'\x89', b'\x8a', b'\x8b', b'\x8c', b'\x8d', b'\x8e', b'\x8f', b'\x90', b'\x91',
+               b'\x92', b'\x93', b'\x94', b'\x95', b'\x96', b'\x97', b'\x98', b'\x99', b'\x9a', b'\x9b', b'\x9c',
+               b'\x9d', b'\x9e', b'\x9f', b'\xa0', b'\xa1', b'\xa2', b'\xa3', b'\xa4', b'\xa5', b'\xa6', b'\xa7',
+               b'\xa8', b'\xa9', b'\xaa', b'\xab', b'\xac', b'\xad', b'\xae', b'\xaf', b'\xb0', b'\xb1', b'\xb2',
+               b'\xb3', b'\xb4', b'\xb5', b'\xb6', b'\xb7', b'\xb8', b'\xb9', b'\xba', b'\xbb', b'\xbc', b'\xbd',
+               b'\xbe', b'\xbf', b'\xc0', b'\xc1', b'\xc2', b'\xc3', b'\xc4', b'\xc5', b'\xc6', b'\xc7', b'\xc8',
+               b'\xc9', b'\xca', b'\xcb', b'\xcc', b'\xcd', b'\xce', b'\xcf', b'\xd0', b'\xd1', b'\xd2', b'\xd3',
+               b'\xd4', b'\xd5', b'\xd6', b'\xd7', b'\xd8', b'\xd9', b'\xda', b'\xdb', b'\xdc', b'\xdd', b'\xde',
+               b'\xdf', b'\xe0', b'\xe1', b'\xe2', b'\xe3', b'\xe4', b'\xe5', b'\xe6', b'\xe7', b'\xe8', b'\xe9',
+               b'\xea', b'\xeb', b'\xec', b'\xed', b'\xee', b'\xef', b'\xf0', b'\xf1', b'\xf2', b'\xf3', b'\xf4',
+               b'\xf5', b'\xf6', b'\xf7', b'\xf8', b'\xf9', b'\xfa', b'\xfb', b'\xfc', b'\xfd', b'\xfe', b'\xff']
 
 # Detect cmp types
 TYPE_DEFAULT = 60
@@ -224,7 +229,7 @@ GEN_TRACEBC_SUFFIX = "_trace.bc"
 GEN_CSV_HEADERS = "filename,time,duration,content,stdout,stderr\n"
 
 '''Mutator'''
-MUT_STR = "AAABAAAC"
+MUT_STR = b"AAABAAAC"
 MUT_MATCH = 4  # Truncate 4 bytes as a fast variant flag for fast matching.
 MUT_STEP = 4  # step size of mutant seeds
 SEED_INIT = 100
--- fuzzer_module/Mutator.py
+++ fuzzer_module/Mutator.py
@@ -14,16 +14,16 @@ def getFillStr(length: int) -> bytes:
     else:
         while len(fill_bytes) <= length:
             for cent in range(1, 4):
-                for c_i in HEX_ASCII[65:91]:
+                for c_i in BYTES_ASCII[65:91]:
                     if len(fill_bytes) >= length:  # Each 100 characters judge it.
                         return fill_bytes[0:length]
-                    for c_j in HEX_ASCII[65:91]:
+                    for c_j in BYTES_ASCII[65:91]:
                         if c_i != c_j:
-                            fill_bytes += bytes.fromhex(c_i) * (4 - cent) + bytes.fromhex(c_j) * cent
+                            fill_bytes += c_i * (4 - cent) + c_j * cent
     return fill_bytes
 
 
-def mutSeeds(seedcont: str, filepath: str, label: str) -> 'list[StructSeed]':
+def mutSeeds(seedcont: bytes, filepath: str, label: str) -> 'list[StructSeed]':
     """
     Replace and add strings for variant input according to the sliding window.
     @param seedcont:
@@ -37,22 +37,22 @@ def mutSeeds(seedcont: str, filepath: str, label: str) -> 'list[StructSeed]':
     endnum = MUT_STEP + seed_len % MUT_STEP
     for i in range(0, seed_len - endnum, MUT_STEP):
         mutate_listq.append(StructSeed(filepath + getMutfilename(label),
-                                       str(seedcont[0:i] + MUT_STR + seedcont[i + len(MUT_STR):seed_len]),
+                                       seedcont[0:i] + MUT_STR + seedcont[i + len(MUT_STR):seed_len],
                                        MUT_SEED_SUB,
                                        set([idx for idx in range(i, i + len(MUT_STR) + 1)])))
     mutate_listq.append(StructSeed(filepath + getMutfilename(label),
-                                   str(seedcont[0:seed_len - endnum] + MUT_STR[0:endnum]),
+                                   seedcont[0:seed_len - endnum] + MUT_STR[0:endnum],
                                    MUT_SEED_SUB,
                                    set([idx for idx in range(seed_len - endnum, seed_len + 1)])))
 
     # Insert byte for seed variation
     for i in range(0, seed_len, MUT_STEP):
         mutate_listq.append(StructSeed(filepath + getMutfilename(label),
-                                       str(seedcont[0:i] + MUT_STR + seedcont[i:seed_len]),
+                                       seedcont[0:i] + MUT_STR + seedcont[i:seed_len],
                                        MUT_SEED_INSERT,
                                        set([idx for idx in range(i, i + len(MUT_STR) + 1)])))
     mutate_listq.append(StructSeed(filepath + getMutfilename(label),
-                                   str(seedcont + MUT_STR),
+                                   seedcont + MUT_STR,
                                    MUT_SEED_INSERT,
                                    set([idx for idx in range(seed_len, seed_len + len(MUT_STR) + 1)])))
     # print(mutate_listq)
@@ -66,12 +66,12 @@ def mutSelectChar(seedcont: bytes, filepath: str, label: str, mutloc_list: list)
     """
     mut_bytes = getFillStr(len(mutloc_list))
     for i, loci in enumerate(mutloc_list):
-        seedcont = seedcont[0:loci] + mut_bytes[i:i+1] + seedcont[loci + 1:]
+        seedcont = seedcont[0:loci] + mut_bytes[i:i + 1] + seedcont[loci + 1:]
     temp_one: StructSeed = StructSeed(filepath + getMutfilename(label), seedcont, MUT_SEED_SUB, set(mutloc_list))
     return temp_one
 
 
-def mutOneChar(seedcont: str, filepath: str, label: str, loc_list) -> StructSeed:
+def mutOneChar(seedcont: bytes, filepath: str, label: str, loc_list) -> StructSeed:
     """
     @param seedcont:
     @param filepath:
@@ -79,32 +79,29 @@ def mutOneChar(seedcont: str, filepath: str, label: str, loc_list) -> StructSeed
     @param loc_list:
     @return: only one seed
     """
-    seedloc_list = list(seedcont)
     for i, loci in enumerate(loc_list):
         while True:
-            tempc = chr(random.randint(48, 122))
-            if seedloc_list[loci] != tempc:
-                seedloc_list[loci] = tempc
+            randi = random.randint(48, 122)
+            tempc = BYTES_ASCII[randi]
+            if seedcont[loci: loci + 1] != tempc:
+                seedcont = seedcont[0:loci] + tempc + seedcont[loci + 1:]
                 break
         # seedloc_list[loci] = chr(255 - ord(seedloc_list[loci]))
-    seedcont = ''.join(seedloc_list)
-    temp_one = StructSeed(filepath + getMutfilename(label), str(seedcont), MUT_SEED_SUB, set(loc_list))
+    temp_one = StructSeed(filepath + getMutfilename(label), seedcont, MUT_SEED_SUB, set(loc_list))
     return temp_one
 
 
-def mutLocFromMap(seedcont: str, filepath: str, label: str, locmapdet_dict: 'dict[int:str]') -> StructSeed:
+def mutLocFromMap(seedcont: str, filepath: str, label: str, locmapdet_dict: 'dict[int:bytes]') -> StructSeed:
     if locmapdet_dict:
-        seedloc_list = list(seedcont)
         for lockey, chgval in locmapdet_dict.items():
-            seedloc_list[lockey] = chgval
-        seedcont = ''.join(seedloc_list)
-        temp_one = StructSeed(filepath + getMutfilename(label), str(seedcont), MUT_SEED_SUB, set(locmapdet_dict))
+            seedcont = seedcont[0:lockey] + chgval + seedcont[lockey + 1:]
+        temp_one = StructSeed(filepath + getMutfilename(label), seedcont, MUT_SEED_SUB, set(locmapdet_dict))
     else:
         temp_one = None
     return temp_one
 
 
-def mutAddLength(seedcont: str, filepath: str, label: str, expand):
+def mutAddLength(seedcont: bytes, filepath: str, label: str, expand):
     multicont = seedcont + getFillStr(expand)
     loc_set = set([i for i in range(len(seedcont), len(multicont))])
     temp_one = StructSeed(filepath + getTimeStr() + EXPAND_SEED, multicont, MUT_SEED_INSERT, loc_set)
@@ -112,9 +109,12 @@ def mutAddLength(seedcont: str, filepath: str, label: str, expand):
 
 
 if __name__ == "__main__":
+    # for each in mutSeeds(b"ZZZZZZZZZZZ", "", ""):
+    #     print(each.content)
+    # print(mutSelectChar(b"ZZZZ","","",[0,1,2,3,4,5,6]).content)
+    # print(mutOneChar(b"ZZZZZZZ", "", "", [0]).content)
     # mutate_seed_list = mutateSeeds("12345678123456789", "", "1")
     # print(mutate_seed_list)
     # mutate_seed_list = mutOneChar("1245678123456789", "", "", set([1]))
     # print(mutate_seed_list.content)
     # print(getFillStr(1024))
-    print(mutSelectChar(b"ZZZZ","","",[0,1,2,3,4,5,6]).content)
